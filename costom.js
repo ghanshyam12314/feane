4d5b8fe9d764ada99cc5c9f048446fce08ac0ffb
ghanshyam12314 <153067875+ghanshyam12314@users.noreply.github.com>
2023-12-14 14:43:36 +0530

    Add files via upload

---
+++ costom.js
@@ -0,0 +1,39 @@
+$('#slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dotsEach:1,
+    autoplay:true,
+    
+    responsiveClass:true,
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2
+        }
+    }
+})
